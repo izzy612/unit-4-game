93abe1584522245f4a52fb40e07620630f9e91d8
Isabella Torres <izzy612@gmail.com>
2019-04-02 23:30:06 -0400

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,76 @@
-var win;
-var lost;
+var win = 0;
+var lost = 0;
 var randomResult;
 var counter = 0;
 
-randomResult = Math.floor(Math.random() * 69) + 30;
-console.log(randomResult);
-
-$(".result").html("Random Result: " + randomResult);
-
-
-for (var i = 0; i < 4; i++) {
-  
-  random = Math.floor(Math.random() * 11) + 1;
-  
-  
-  var crystals = $("<div>");
-  crystals.attr("src", "assets/img/crystal1.2.jpg");
-  crystals.attr("src", "assets/img/crystal2.2.png");
-  crystals.attr("src", "assets/img/crystal3.jpg");
-  crystals.attr("src", "assets/img/crystal4.jpg");
-    
-  crystals.attr("random-value", random);
-  console.log("random-value")
-
-  $(".crystals").append(crystals);
-
-}
-
-$(".crystals").on("click", function () {
-  
-  var number = parseInt($(this).attr("random-value"));
-
-  counter += number;
-  
-  console.log(counter)
-
-  alert("New score " + counter);
-
-  if (counter === randomResult) {
-    
-    win++
-    
-    $(".win").html("Wins " + win);
-
-    counter = 0;
-
-
+var reset = function () {
    
-    
+  $(".crystals").empty();
+
+  randomResult = Math.floor(Math.random() * 69) + 30;
+  console.log(randomResult);
+
+  $(".result").text("Random Result: " + randomResult);
+
+
+
+  for (var i = 1; i < 5; i++) {
+
+    random = Math.floor(Math.random() * 11) + 1;
+
+
+    var crystals = $("<img>");
+    crystals.attr("src", `assets/img/crystal${i}.png`);
+    crystals.addClass("crystal-img")
+    crystals.addClass("container")
+    crystals.addClass("col-3")
+    crystals.attr("value", random);
+    $(".crystals").append(crystals);
   }
 
+}
+ reset();
+
+  $(".crystal-img").on("click",  function () {
   
-  
-  
+    var number = parseInt($(this).attr("value"));
+
+    counter += number;
+    console.log(counter)
 
 
+    alert("New score " + counter);
 
+    if (counter === randomResult) {
+    
+      win++;
+    
+      $(".win").text("Wins: " + win);
+      console.log("You Win")
 
+      counter = 0;
+
+      reset();
+
+      
+
+    }
+
+    else if (counter > randomResult) {
+    
+      lost++;
+
+      $(".lost").text("Loses: " + lost);
+      console.log("You Lost")
+
+      counter = 0;
+
+      reset();
+
+      
+
+    }
   
-});
+
+  });
+ 
